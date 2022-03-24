0e7be80753c53a834efff9a512895b48697d14aa
M0diis <modestas.kazlauskas@ktu.edu>
2022-03-24 15:11:52 +0200

    Fix database .env & latlong post via API.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use PDO;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
+    private PDO $pdo;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $dsn = "pgsql:host=".$params->get('database_host').";port=5432;dbname=".$params->get('database_name').";";
+
+        $this->pdo = new PDO($dsn, $params->get('database_user'), $params->get('database_password'), [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    }
+
     /**
      * @Route("/api/v1/submit/latlng", name="location_submit", methods={"POST"})
      *
@@ -20,15 +29,24 @@ class ApiController extends AbstractController
      *
      * @return Response
      */
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
         $lat = $_POST['lat'];
         $lng = $_POST['lng'];
 
-        if(!empty($lat) || !empty($lng))
+        $logger->info("Received latlng: $lat, $lng");
+
+        if(empty($lat) || empty($lng))
         {
             return new Response(400);
         }
+
+        $stmt = $this->pdo->prepare("INSERT INTO \"LocationTest\"(latlong) VALUES (POINT(:lat, :lng))");
+
+        $stmt->bindParam(':lat', $lat);
+        $stmt->bindParam(':lng', $lng);
+
+        $stmt->execute();
 
         return new Response(200);
     }
